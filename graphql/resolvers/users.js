bdab7302726c4d98b280701fc9a32eea42d65ea9
Rafael Tomas <rafaeltomassouza@gmail.com>
2022-05-24 10:49:55 -0300

    fix(login): Don't working

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,17 +2,72 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { UserInputError } = require('apollo-server');
 
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require('../../utils/validators');
 const { KEY_SECRET } = require('../../config');
 const User = require('../../models/User');
 
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      userName: user.userName,
+    },
+    KEY_SECRET,
+    { expiresIn: '1h' },
+  );
+}
+
 module.exports = {
+
   Mutation: {
+    async login(_, { userName, password }) {
+      const { errors, valid } = validateLoginInput(userName, password);
+
+      if (!valid) {
+        throw new UserInputError('Errors', { errors });
+      }
+
+      const user = await User.findOne({ userName });
+
+      if (!user) {
+        errors.general = 'User not found';
+        throw new UserInputError('User not found', { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = 'Wrong crendetials';
+        throw new UserInputError('Wrong crendetials', { errors });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
+
     async register(
       _,
       { registerInput: { userName, email, password, confirmPassword } },
       context,
       info,
     ) {
+      const { valid, errors } = validateRegisterInput(
+        userName,
+        email,
+        password,
+        confirmPassword,
+      );
+      if (!valid) {
+        throw new UserInputError('Errors', { errors });
+      }
       const user = await User.findOne({ userName });
       if (user) {
         throw new UserInputError('UserName is taken', {
@@ -32,15 +87,7 @@ module.exports = {
       });
 
       const res = await newUser.save();
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          userName: res.userName,
-        },
-        KEY_SECRET,
-        { expiresIn: '1h' },
-      );
+      const token = generateToken(res);
 
       return {
         ...res._doc,
