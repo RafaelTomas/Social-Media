67adc7cd24e92e015777dd5953f02d96f0529fff
Rafael Tomas <rafaeltomassouza@gmail.com>
2022-05-26 00:41:00 -0300

    feat: create routes and add semantic ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 function App() {
-  return <h1>Hey world</h1>;
+  return (
+    <Router>
+      <Container>
+      <Route path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+     
+      </Container>
+    </Router>
+  );
 }
 
 export default App;
