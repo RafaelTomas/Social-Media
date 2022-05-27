5057f92c9d38d0ef4652aa7aa23593a9401f1ce1
Rafael Tomas <rafaeltomassouza@gmail.com>
2022-05-27 19:04:48 -0300

    feat: create routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,30 @@
-import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import { Container } from 'semantic-ui-react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import ApolloProvider from './ApolloProvider';
+import 'semantic-ui-css/semantic.min.css'
+import "./index.css";
 
-ReactDOM.render(ApolloProvider, document.getElementById('root'));
+import { App } from "./templates/App";
+import { Register } from "./templates/Register";
+import { Login } from "./templates/Login";
+import Nav from './components/Nav';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Container>
+        <Nav/>
+      <Routes>
+        <Route exact path='/' component={<App/>} />
+        <Route exact path='/login' component={<Login/>} />
+        <Route exact path='/register' component={<Register/>} />
+      </Routes>
+    </Container>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+
---
+++ client/src/templates/App/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+import './index.css';
+
+
+function App() {
+  return (
+    <div><h1>Home</h1></div>
+  );
+}
+
+
+export default App;
